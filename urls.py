dedd6cef60f4a0a3c05f24642d367c5b3bc771ff
nachopro <tranceway@gmail.com>
2012-05-24 11:02:41 -0300

    soporte para media serve en instancia de desarrollo y redirección automática a contacts

--- urls.py
+++ urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.conf import settings
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'agenda_hx.views.home', name='home'),
@@ -12,7 +14,21 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    (r'^$', 'django.views.generic.simple.redirect_to', {'url': '/contacts/'}),
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
     (r'^contacts/', include('contacts.urls')),
+
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+   )
+
+if settings.DEBUG:
+    urlpatterns += patterns('django.contrib.staticfiles.views',
+        url(r'^static/(?P<path>.*)$', 'serve'),
+    )
